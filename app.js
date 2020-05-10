19b33b312b6fafe1ed905366fc77c213d19256df
hello-mole <qnsk0129@gmail.com>
2020-05-10 23:30:59 +0900

    draw Weather with Nomad Coders lec

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const today = document.querySelector(".js-today");
 
-function init(){
+function drawToday(){
     const now = new Date();
     const week = ["일", "월", "화", "수", "목", "금", "토", "일"];
     const month = now.getMonth()+1;
@@ -8,6 +8,10 @@ function init(){
     const day = now.getDay();
     
     today.innerText = `${month}월 ${date}일 ${week[day]}: 오늘의 할 일`
+}
+
+function init(){
+    drawToday();
 }
 
 init()
---
+++ weather.js
@@ -0,0 +1,54 @@
+const weather = document.querySelector(".js-weather");
+
+const COORDS = 'coords';
+const API_KEY = "2b3a694d2b1e39629613a0a3743e40d5";
+
+function getWeather(lat, lon){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json()
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `여기는 ${place}, 지금 기온은 ${Math.round(temperature)}°C`
+    })
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError(){
+    console.log("Can't access geo loaction");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    } else {
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
